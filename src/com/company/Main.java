53c82529c6f5a246ad6bb2e7201bea999d5d44f5
shannoncantech <LdwpEtegOLLon9#1>
2018-05-17 14:48:29 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String firstName = "Shannon";
+        String lastName = "Foster";
+        String fullName = firstName + " " + lastName;
+        System.out.println("Hello, my name is " + fullName + ".");
+        System.out.println("There are " + fullName.length() + " letters in my name.");
+    }
+}
